5e090c5267b8a54565b32bed0df51e3c4af98899
Masum Billal <2643770+proafxin@users.noreply.github.com>
2023-01-12 03:23:50 +0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""Define views"""
 
-# Create your views here.
+from rest_framework import generics
+
+from app import models, serializers
+
+
+class PersonListView(generics.ListCreateAPIView):
+    """Class based list view for Person"""
+
+    serializer_class = serializers.PersonSerializer
+    queryset = models.Person.objects.all()
+
+
+class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Class based detail view for Person"""
+
+    serializer_class = serializers.PersonSerializer
+    queryset = models.Person.objects.all()
